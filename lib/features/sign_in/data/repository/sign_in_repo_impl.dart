b0a95a2a502f1297d5cbaedf5008bf22bef1230a
KhaledElalfy1 <khaledalfy53@gmail.com>
2025-07-05 10:19:20 +0300

    cache user id, email, image, and user name

--- lib/features/sign_in/data/repository/sign_in_repo_impl.dart
+++ lib/features/sign_in/data/repository/sign_in_repo_impl.dart
@@ -1,10 +1,13 @@
 import 'package:dartz/dartz.dart';
+import 'package:jwt_decode/jwt_decode.dart';
 import 'package:patient_app/core/database/api/api_consumer.dart';
 import 'package:patient_app/core/database/api/api_error_handler.dart';
 import 'package:patient_app/core/database/api/api_error_model.dart';
 import 'package:patient_app/core/database/api/end_points.dart';
 import 'package:patient_app/core/database/cache/cache_keys.dart';
+import 'package:patient_app/core/database/cache/cashe_helper.dart';
 import 'package:patient_app/core/database/cache/secure_storage.dart';
+import 'package:patient_app/core/services/get_it.dart';
 import 'package:patient_app/features/sign_in/data/models/sign_in_request_model.dart';
 import 'package:patient_app/features/sign_in/data/models/sign_in_response_model.dart';
 import 'package:patient_app/features/sign_in/data/repository/sign_in_repo.dart';
@@ -22,7 +25,7 @@ class SignInRepoImpl implements SignInRepo {
           data: signInRequestModel.toJson());
       final signInResponse = SignInResponseModel.fromJson(response.data);
       if (response.statusCode == 200) {
-      await  _cacheTokenAndId(signInResponse.token);
+        await _cacheTokenAndId(signInResponse.token);
         return const Right(null);
       } else {
         return Left(ApiErrorModel(
@@ -35,9 +38,16 @@ class SignInRepoImpl implements SignInRepo {
     }
   }
 
-  Future _cacheTokenAndId(String token) async{
-   await SecureStorage.instance.addData(key: CacheKeys.token, data: token);
-  //  Map<String, dynamic> payload = Jwt.parseJwt(token);
-  //  await SecureStorage.instance.addData(key: CacheKeys.id, data: data)
+  Future _cacheTokenAndId(String token) async {
+    await SecureStorage.instance.addData(key: CacheKeys.token, data: token);
+    Map<String, dynamic> payload = Jwt.parseJwt(token);
+    await SecureStorage.instance
+        .addData(key: CacheKeys.id, data: payload['sub']);
+    await getIt<CacheHelper>()
+        .saveData(key: CacheKeys.profileImage, value: payload['Image']);
+    await getIt<CacheHelper>()
+        .saveData(key: CacheKeys.username, value: payload['User Name']);
+    await getIt<CacheHelper>()
+        .saveData(key: CacheKeys.email, value: payload['email']);
   }
 }
--- lib/patient_app.dart
+++ lib/patient_app.dart
@@ -51,7 +51,7 @@ class PatientApp extends StatelessWidget {
               themeMode: context.watch<ChangeThemesCubit>().isDarkMode
                   ? ThemeMode.dark
                   : ThemeMode.light,
-              initialRoute: Routing.main,
+              initialRoute: Routing.splash,
               onGenerateRoute: AppRouters().generateRoute,
             );
           },
